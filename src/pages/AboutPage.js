bb4e1ab45d67edc4ac9412584b31657bc7ab1e9b
Dibya Ranjan <dibyaranjan576@gmail.com>
2022-10-28 10:39:56 +0530

    filter functionality finished

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            nam amet maxime id odit aliquid facilis illo est vitae? Ex doloribus
+            non distinctio quis debitis iste saepe dolorum est id. Lorem, ipsum
+            dolor sit amet consectetur adipisicing elit. Perferendis maxime
+            necessitatibus autem quasi qui impedit sed temporibus iste neque
+            quidem.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,100 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from "../actions";
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+    return {
+      ...state,
+      allProducts: [...action.payload],
+      filtered_products: [...action.payload],
+      filters: {
+        ...state.filters,
+        max_price: maxPrice,
+        price: maxPrice,
+      },
+    };
+  }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let tempProducts = [...filtered_products];
+    if (sort === "price-lowest") {
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+    }
+    if (sort === "price-highest") {
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+    }
+    if (sort === "name-a") {
+      tempProducts = tempProducts.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    if (sort === "name-z") {
+      tempProducts = tempProducts.sort((a, b) => b.name.localeCompare(a.name));
+    }
+    return { ...state, filtered_products: tempProducts };
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    const { allProducts } = state;
+    const { text, company, color, price, shipping, category } = state.filters;
+    let tempProducts = [...allProducts];
+    if (text) {
+      tempProducts = tempProducts.filter((item) => {
+        return item.name.toLowerCase().includes(text);
+      });
+    }
+    if (category !== "all") {
+      tempProducts = tempProducts.filter((item) => item.category === category);
+    }
+    if (company !== "all") {
+      tempProducts = tempProducts.filter((item) => item.company === company);
+    }
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((item) =>
+        item.colors.find((c) => c === color)
+      );
+    }
 
-export default filter_reducer
+    if (shipping) {
+      tempProducts = tempProducts.filter((item) => {
+        return item.shipping;
+      });
+    }
+
+    tempProducts = tempProducts.filter((item) => item.price <= price);
+
+    return { ...state, filtered_products: tempProducts };
+  }
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      filters: {
+        ...state.filters,
+        text: "",
+        company: "all",
+        category: "all",
+        color: "all",
+        price: state.filters.max_price,
+        shipping: false,
+      },
+    };
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
